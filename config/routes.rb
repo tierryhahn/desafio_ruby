f78ad634a953d6cb58c45c31b5342c5316035873
Tierry <tierryhahn@gmail.com>
2024-08-18 20:18:48 -0300

    feat/sendMailCheck

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   post '/users/sign_in_with_otp', to: 'users#sign_in_with_otp'
   post 'automations/request_link', to: 'automations#request_link'
   patch 'users/update_account_ids', to: 'users#update_account_ids'
+  post 'automations/send_linkedin_message', to: 'automations#send_linkedin_message'
+  post 'automations/send_email', to: 'automations#send_email'
+  get 'attendees', to: 'automations#list_attendees'
 end

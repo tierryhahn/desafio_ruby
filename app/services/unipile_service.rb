456600845219490f519816fda96016b5f1f49ef6
Tierry <tierryhahn@gmail.com>
2024-08-20 18:17:26 -0300

    fix/credentialsENV

--- app/services/unipile_service.rb
+++ app/services/unipile_service.rb
@@ -1,19 +1,24 @@
+require 'net/http'
+require 'json'
+require 'uri'
+require 'dotenv/load'
+
 class UnipileService
-  API_BASE_URL = "https://api6.unipile.com:13646/api/v1"
+  API_BASE_URL = ENV['UNIPILE_API_BASE_URL']
 
   def initialize(user = nil)
     @user = user
   end
 
   def request_automation_link(email, otp_token)
-    uri = URI("#{API_BASE_URL}/hosted/accounts/link")
+    uri = URI.parse("#{API_BASE_URL}/hosted/accounts/link")
     http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
+    http.use_ssl = (uri.scheme == 'https')
 
     request = Net::HTTP::Post.new(uri.request_uri)
     request["Accept"] = "application/json"
     request["Content-Type"] = "application/json"
-    request["X-API-KEY"] = "Fy+jNMsR.oPh59qIU9ukBmH6QROHfYjntPKkmB0e4xnPe9X4cqB8="
+    request["X-API-KEY"] = ENV['UNIPILE_API_KEY']
 
     request.body = {
       expiresOn: "2025-12-31T23:59:59.999Z",
@@ -21,7 +26,7 @@ class UnipileService
       success_redirect_url: "https://example.com/success",
       failure_redirect_url: "https://example.com/failure",
       notify_url: "https://example.com/notify",
-      disabled_features: ["linkedin_recruiter"],  
+      disabled_features: ["linkedin_recruiter"],
       api_url: "https://api6.unipile.com:13646",
       sync_limit: {
         MAILING: "NO_HISTORY_SYNC",
@@ -39,15 +44,15 @@ class UnipileService
   end
 
   def start_linkedin_chat(account_id, text, attendees_ids, subject = nil, voice_message = nil, attachments = [])
-    uri = URI("#{API_BASE_URL}/chats")
+    uri = URI.parse("#{API_BASE_URL}/chats")
     http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-  
+    http.use_ssl = (uri.scheme == 'https')
+
     request = Net::HTTP::Post.new(uri.request_uri)
     request["Accept"] = "application/json"
     request["Content-Type"] = "application/json"
-    request["X-API-KEY"] = "Fy+jNMsR.oPh59qIU9ukBmH6QROHfYjntPKkmB0e4xnPe9X4cqB8="
-  
+    request["X-API-KEY"] = ENV['UNIPILE_API_KEY']
+
     body = {
       account_id: account_id,
       text: text,
@@ -56,21 +61,21 @@ class UnipileService
       voice_message: voice_message,
       attachments: attachments
     }.compact
-  
+
     request.body = body.to_json
     response = http.request(request)
     JSON.parse(response.body)
   end
 
   def send_chat_message(chat_id, text, thread_id = nil)
-    uri = URI("#{API_BASE_URL}/chats/#{chat_id}/messages")
+    uri = URI.parse("#{API_BASE_URL}/chats/#{chat_id}/messages")
     http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
+    http.use_ssl = (uri.scheme == 'https')
 
     request = Net::HTTP::Post.new(uri.request_uri)
     request["Accept"] = "application/json"
     request["Content-Type"] = "application/json"
-    request["X-API-KEY"] = "Fy+jNMsR.oPh59qIU9ukBmH6QROHfYjntPKkmB0e4xnPe9X4cqB8="
+    request["X-API-KEY"] = ENV['UNIPILE_API_KEY']
 
     body = {
       text: text,
@@ -83,15 +88,15 @@ class UnipileService
   end
 
   def send_email(account_id, from, to, subject, body, cc = [], bcc = [], reply_to = nil, custom_headers = [], tracking_options = {}, attachments = [])
-    uri = URI("#{API_BASE_URL}/emails")
+    uri = URI.parse("#{API_BASE_URL}/emails")
     http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    
+    http.use_ssl = (uri.scheme == 'https')
+
     request = Net::HTTP::Post.new(uri.request_uri)
     request["Accept"] = "application/json"
     request["Content-Type"] = "application/json"
-    request["X-API-KEY"] = "Fy+jNMsR.oPh59qIU9ukBmH6QROHfYjntPKkmB0e4xnPe9X4cqB8="
-    
+    request["X-API-KEY"] = ENV['UNIPILE_API_KEY']
+
     request.body = {
       account_id: account_id,
       from: from,
@@ -105,41 +110,15 @@ class UnipileService
       tracking_options: tracking_options,
       attachments: attachments
     }.to_json
-    
+
     response = http.request(request)
     JSON.parse(response.body)
   end
 
   def list_attendees
-    uri = URI("#{API_BASE_URL}/chat_attendees")
+    uri = URI.parse("#{API_BASE_URL}/chat_attendees")
     http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-
-    request = Net::HTTP::Get.new(uri.request_uri)
-    request["Accept"] = "application/json"
-    request["X-API-KEY"] = "Fy+jNMsR.oPh59qIU9ukBmH6QROHfYjntPKkmB0e4xnPe9X4cqB8="
-
-    response = http.request(request)
-    JSON.parse(response.body)
-  end
-
-  def list_chats
-    uri = URI("#{API_BASE_URL}/chats")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-
-    request = Net::HTTP::Get.new(uri.request_uri)
-    request["Accept"] = "application/json"
-    request["X-API-KEY"] = "Fy+jNMsR.oPh59qIU9ukBmH6QROHfYjntPKkmB0e4xnPe9X4cqB8="
-
-    response = http.request(request)
-    JSON.parse(response.body)
-  end
-
-  def create_linkedin_connections(cursor, limit, account_id)
-    uri = URI("#{API_BASE_URL}/users/relations?cursor=#{cursor}&limit=#{limit}&account_id=#{account_id}")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
+    http.use_ssl = (uri.scheme == 'https')
 
     request = Net::HTTP::Get.new(uri.request_uri)
     request["Accept"] = "application/json"
@@ -148,4 +127,18 @@ class UnipileService
     response = http.request(request)
     JSON.parse(response.body)
   end
+
+  def list_chats
+    uri = URI.parse("#{API_BASE_URL}/chats")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == 'https')
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request["Accept"] = "application/json"
+    request["X-API-KEY"] = ENV['UNIPILE_API_KEY']
+
+    response = http.request(request)
+    JSON.parse(response.body)
+  end
+
 end
